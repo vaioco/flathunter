29b862f9544a3aabf42fd7d2e5e273fb2e4cdd20
pomeloy <45542782+pomeloy@users.noreply.github.com>
2024-04-02 20:46:33 +0200

    Add images to Apprise notification

--- flathunter/notifiers/sender_apprise.py
+++ flathunter/notifiers/sender_apprise.py
@@ -11,6 +11,7 @@ class SenderApprise(Processor, Notifier):
     def __init__(self, config):
         self.config = config
         self.apprise_urls = self.config.get('apprise', {})
+        self.__notify_with_images: bool = self.config.apprise_notify_with_images()
 
     def process_expose(self, expose):
         """Send a message to a user describing the expose"""
@@ -34,14 +35,16 @@ class SenderApprise(Processor, Notifier):
             address=expose.get('address', 'N/A'),
             durations=expose.get('durations', 'N/A')
         ).strip()
-        self.__send_msg(message, title)
+        images = expose.get('images', expose.get('image'))\
+            if self.__notify_with_images else None
+        self.__send_msg(message, title, images)
         return expose
 
     def notify(self, message: str):
         """ Send the given message to users """
-        self.__send_msg(message=message, title=None)
+        self.__send_msg(message=message, title=None, images=None)
 
-    def __send_msg(self, message, title):
+    def __send_msg(self, message, title, images):
         """Send messages to each of the Apprise urls"""
         apobj = apprise.Apprise()
         if self.apprise_urls is None:
@@ -52,5 +55,6 @@ class SenderApprise(Processor, Notifier):
         apobj.notify(
             body=message,
             title=title,
+            attach=images,
             body_format=apprise.NotifyFormat.TEXT,
         )
